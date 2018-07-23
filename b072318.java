16dffd5dba2aade4bcde3b0f70494cef68d60f33
Ben <bentube0117@gmail.com>
2018-07-23 12:59:08 -0400

    upload

---
+++ b072318.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class b072318
+{
+	public static void main(String[] args)
+	{
+		JOptionPane.showMessageDialog(null,
+			"EYY!", "The Java File - Beta 072318",
+			JOptionPane.INFORMATION_MESSAGE);
+	}
+}
---
+++ tjf0x2.java
@@ -0,0 +1,10 @@
+public class tjf0x2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is The Java File!");
+		System.out.println("This has Java stuff.");
+		System.out.println("");
+		System.out.println("The Java File - v. 0.2");
+	}
+}
