a481f8e28fc8795cdadc62926e4c5b2af0bc3e4d
Ben <bentube0117@gmail.com>
2018-07-23 12:42:02 -0400

    original

---
+++ README.md
@@ -0,0 +1,7 @@
+# javafile
+This is The Java File.
+
+The Java File is a Java program.
+I don't need to explain how to use Java becuase this is GitHub.
+
+I have added the .class file just in case you need it.
--- tjf0.0.1/tjf001.java
+++
@@ -1,9 +0,0 @@
-public class tjf001
-{
-	public static void main(String[] args)
-	{
-		System.out.println("The Java File");
-		System.out.println("by Ben Chase YouTube");
-		System.out.println("Version 0.0.1");
-	}
-}
