0688f7150aa65b1cd061b74e08014446686a52f5
Ben0117 <41578881+Ben0117@users.noreply.github.com>
2018-07-24 18:08:28 -0400

    NEW BETA

---
+++ Betas/Beta 1.0/b072418.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class b072418
+{
+	public static void main(String[] args)
+	{
+  
+  System.out.println("starting up");
+  
+		JOptionPane.showMessageDialog(null,
+			"EYY!", "The Java File - Beta 072418",
+			JOptionPane.INFORMATION_MESSAGE);
+	}
+}
