580ea35d24c3fef0d5aed9b5ecc92321a77cc070
Ben0117 <41578881+Ben0117@users.noreply.github.com>
2018-08-12 14:37:50 -0400

    Add files via upload

---
+++ Releases/tjf1x0.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class tjf1x0
+{
+	public static void main(String[] args)
+	{
+  
+  	System.out.println("starting up");
+  
+		JOptionPane.showMessageDialog(null,
+			"Welcome to The Java File! Version 1.0 is finally here!", "The Java File - Version 1.0",
+			JOptionPane.INFORMATION_MESSAGE);
+	}
+}
